11f12b059f66b2bd5253482949bac1b87474d855
元木嵩人 <kmnernofkingdeep@gmail.com>
2018-02-14 18:23:43 +0900

    Initial Commit

---
+++ SNSBusinessCards/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SNSBusinessCards
+//
+//  Created by 元木嵩人 on 2018/02/14.
+//  Copyright © 2018年 元木嵩人. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
