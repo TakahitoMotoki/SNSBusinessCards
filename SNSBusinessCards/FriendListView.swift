1613f6e5a925298e724e540137d425b561b75ea0
元木嵩人 <kmnernofkingdeep@gmail.com>
2018-02-19 15:59:43 +0900

    Edited FriendListView

--- SNSBusinessCards/FriendListView.swift
+++ SNSBusinessCards/FriendListView.swift
@@ -8,9 +8,12 @@
 
 import Foundation
 import UIKit
+import FontAwesomeKit
 
 class FriendListView: UIView {
-    let tableView: UITableView
+    var friendLabel: UILabel
+    let addButton: UIButton
+    var tableView: UITableView
     let NATIVE_BOUND_SIZE = UIScreen.main.bounds.size
     // let refreshControl: UIRefreshControl
     
@@ -19,11 +22,40 @@ class FriendListView: UIView {
     }
 
     required init(model: FriendListViewModel) {
-        tableView = UITableView(frame: CGRect(x: 0, y: 0, width: NATIVE_BOUND_SIZE.width - 100 , height: NATIVE_BOUND_SIZE.height), style: .plain)
-        // refreshControl = UIRefreshControl()
-        // tableView.addSubview(refreshControl)
+        friendLabel = UILabel(frame: CGRect(x: NATIVE_BOUND_SIZE.width / 2 - 100, y: 18, width: 200, height: 44))
+        addButton = UIButton()
+        tableView = UITableView(frame: CGRect(x: 0, y: 70, width: NATIVE_BOUND_SIZE.width , height: NATIVE_BOUND_SIZE.height - 100), style: .plain)
+        super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         
-        super.init(frame: CGRect(x: 0, y: 0, width: 0, height: 0));
+        // MARK: friendLabel Start
+        friendLabel.textAlignment = .center
+        friendLabel.font = UIFont(name: "Gurmukhi MN", size: 22)
+        friendLabel.text = "Friend: 100"
+        friendLabel.textColor = UIColor(red: 200.0 / 255.0, green: 0.0 / 255.0, blue: 200.0 / 255.0, alpha: 1.0)
+        self.addSubview(friendLabel)
+        // MARK: friendLabel End
+
+        // MARK: addButton Start
+        let buttonFontImage = FAKFontAwesome.userIcon(withSize: 32.0)
+        buttonFontImage?.addAttribute(NSAttributedStringKey.foregroundColor.rawValue, value: UIColor.white)
+        let buttonImage = buttonFontImage?.image(with: CGSize(width: 32.0, height: 32.0))
+        
+        addButton.backgroundColor = UIColor.clear
+        addButton.setBackgroundImage(buttonImage, for: .normal)
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(addButton)
+
+        addButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20.0).isActive = true
+        addButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 25.0).isActive = true
+        addButton.widthAnchor.constraint(equalToConstant: 32.0).isActive = true
+        addButton.heightAnchor.constraint(equalToConstant: 32.0).isActive = true
+        // MARK: addButton End
+        
+        // MARK: tableView Start
         self.addSubview(tableView)
+        // MARK: tableView End
+        
+        // Backgrond Color
+        self.backgroundColor = UIColor(red: 255.0 / 255.0, green: 255.0 / 255.0, blue: 0.0 / 255.0, alpha: 0.9)
     }
 }
